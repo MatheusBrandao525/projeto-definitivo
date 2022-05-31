a8926d1e4a85c3dfce900bd28dc8958da846e3b4
Matheus Felipe Brandão da Silva <77033014+MatheusBrandao525@users.noreply.github.com>
2022-05-31 10:14:19 -0300

    refatoração, retirando tags desnecessarias.

--- cadastro_cliente.php
+++ cadastro_cliente.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Loja</title>
 
-</head>
-<body>
-	
 <?php
 session_start();
 	// Incluindo arquivos importantes...
@@ -108,6 +98,3 @@ session_start();
 		// Incluindo arquivo de Rodapé... 
 		include 'footer.php' 
 	?>
-	
-</body>
-</html>
