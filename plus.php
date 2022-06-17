4e8ac4cc11c2735a6e775add6df8aa74b45421e6
MatheusBrandao525 <mafe123silva@gmail.com>
2022-06-17 10:27:04 -0400

    adicionando evento de aumentar quantidade no carrinho

--- plus.php
+++ plus.php
@@ -9,49 +9,34 @@
 
 	
 	include 'conexao.php';
-	
-	$consulta = $cn->query("SELECT * FROM tbl_produto WHERE id_produto = '$id_produto' ");
-	$exibe = $consulta->fetch(PDO::FETCH_ASSOC);
-
-	$qnt_estoque = $exibe['qnt_estoque'];
 
 
 	// verificando se o codigo do produto NÃO está vazio
-	if (!empty($_GET['id'])) {
+
 	
 	// inserindo o código do produto na variável $id_produto
-	
-	$id_produto=$_GET['id'];
-	// se a sessão carrinho não estiver preenchida
-	if (!isset($_SESSION['carrinho'])) {
-		  // será criado uma sessão chamado carrinho para receber um vetor
-		  $_SESSION['carrinho'] = array();
-	}
-	
-	// se a variavel $id_produto não estiver setado(preenchida)
-	if (!isset($_SESSION['carrinho'][$id_produto])) {
-		
-		//  adicionado um produto ao carrinho
-		$_SESSION['carrinho'][$id_produto]=1;
-	}else {
-		// se setada adiciona novos produtos
-		  $_SESSION['carrinho'][$id_produto]+=1;
-          header('location:carrinho.php');
 
-	}
+	$codigoUsuario = $_GET['idUser'];
+	$codigoProduto = $_GET['id'];
 
-		// incluindo o arquivo 'mostraCarrinho.php'
-		header('location:carrinho.php');
-		
-	} else {
-		
-		//mostrando o carrinho	vazio	
-		header('location:carrinho.php');
-		
-		
-	}	
+	$consultaEstoque = $cn->query("SELECT qnt_estoque FROM tbl_produto WHERE id_produto = '$codigoProduto'");
+	$exibeEstoque = $consultaEstoque->fetch(PDO::FETCH_ASSOC);
 
+	$consultaQuantidade = $cn->query("SELECT * FROM tbl_carrinho WHERE codigo_produto = '$codigoProduto' AND codigo_usuario = '$codigoUsuario'");
+	$exibeQuantidadeCart = $consultaQuantidade->fetch(PDO::FETCH_ASSOC);
 
+	$quantidadeEmEstoque = $exibeEstoque['qnt_estoque'];
+	$quantidadeNocarrinho = $exibeQuantidadeCart['quantidade_produto'];
+
+	if($consultaQuantidade->rowCount()>0){
+ 			if($quantidadeNocarrinho >=1 && $quantidadeNocarrinho < $quantidadeEmEstoque){
+				$diminui = $cn->query("UPDATE tbl_carrinho SET quantidade_produto = quantidade_produto +1 WHERE codigo_usuario = '$codigoUsuario' AND codigo_produto = '$codigoProduto';");
+				header('location:carrinho.php');
+				exit();
+			}elseif($quantidadeNocarrinho == $quantidadeEmEstoque){
+				echo 'impossivel adicionar mais produtos!';
+			}
+		} 
 
 
 
