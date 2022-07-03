d030dde25a6c72ed5c63dc12a07e7da640e5b366
MatheusBrandao525 <mafe123silva@gmail.com>
2022-07-02 22:19:05 -0400

    começando a trabalar a pagina de lista de desejos

--- listaDesejos.php
+++ listaDesejos.php
@@ -7,56 +7,66 @@ include 'menu-principal.php';
 ?>
 
 
-<div class="row dadosCarrinho" style="margin-top: 15px;">
+<div class="row dadosWishList" style="margin: 15px 15px; display: block;">
 
-            <div class="col-sm-1 col-sm-offset-1">
-                <h4>IMAGEM</h4>
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-sm-1 col-sm-offset-1">
+                        <h4>IMAGEM</h4>
+                    </div>
+                    
+                    
+                    <div class="col-sm-4 margin">
+                        <h4>NOME PRODUTO</h4>
+                    </div>	
+                    
+                    
+                    <div class="col-sm-2">
+                        <h4>VALOR DO PRODUTO</h4>
+                    </div>		
+            
+                    <div class="col-sm-3">
+
+                            <h4 class="qntDisponivel text-center" id="#qntcart">QUANTIDADE DISPONIVEL</h4>
+                        
+                    </div>
+            
+                    <div class="col-sm-2 col-xs-offset-right-1">
+                    </div> 
+                </div>
             </div>
+
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-sm-1 col-sm-offset-1">
+                        <img src="imgem/" class="img-responsiva">
+                    </div>
+                    
+                    
+                    <div class="col-sm-4 margin">
+                        <h4>NOME PRODUTO</h4>
+                    </div>	
+                    
+                    
+                    <div class="col-sm-2">
+                        <h4>VALOR DO PRODUTO</h4>
+                    </div>		
             
+                    <div class="col-sm-3 quantidadeDisponivel">
+
+                        <h4 class="qntDisponivel text-center" id="#qntcart">QUANTIDADE DISPONIVEL</h4>
+
+                    </div>
             
-            <div class="col-sm-4 margin">
-                <h4>NOME PRODUTO</h4>
+                    <div class="col-sm-2 botaoAdicionarAoCarrinhoListaDeDesejos">
+                        <a href="" class="btn btn-block btn-success adc-listaDesejos">Adicionar ao carrinho</a>
+                    </div> 
+                </div>
             </div>	
-            
-            
-            <div class="col-sm-2">
-                <h4>VALOR DO PRODUTO</h4>
-            </div>		
     
-            <div class="col-sm-3">
 
-                    <h4 class="qntDisponivel text-center" id="#qntcart">QUANTIDADE DISPONIVEL</h4>
-                
-            </div>
     
-            <div class="col-sm-2 col-xs-offset-right-1">
-            </div> 
 
-
-			
-            <div class="col-sm-1 col-sm-offset-1">
-                <img src="imgem/" class="img-responsiva">
-            </div>
-            
-            
-            <div class="col-sm-4 margin nomeProduto">
-                <h4>NOME PRODUTO</h4>
-            </div>	
-            
-            
-            <div class="col-sm-2 precoItem">
-                <h4>VALOR DO PRODUTO</h4>
-            </div>		
-    
-            <div class="col-sm-3 quantidadeDisponivel">
-
-                    <h4 class="qntDisponivel text-center" id="#qntcart">QUANTIDADE DISPONIVEL</h4>
-                
-            </div>
-    
-            <div class="col-sm-2 col-xs-offset-right-1 botaoAdicionarAoCarrinhoListaDeDesejos">
-                <a href="" class="btn btn-block btn-success adc-listaDesejos">Adicionar ao carrinho</a>
-            </div> 
             
 </div>	
         
