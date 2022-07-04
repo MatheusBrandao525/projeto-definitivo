0086ca603897d07830c5e99c66aa8fc620fc56c5
MatheusBrandao525 <mafe123silva@gmail.com>
2022-07-03 20:24:02 -0400

    construcao do layout da pagina lista de desejos

--- listaDesejos.php
+++ listaDesejos.php
@@ -12,49 +12,22 @@ include 'menu-principal.php';
             <div class="col-sm-12">
                 <div class="row">
                     <div class="col-sm-1 col-sm-offset-1">
-                        <h4>IMAGEM</h4>
-                    </div>
-                    
-                    
-                    <div class="col-sm-4 margin">
-                        <h4>NOME PRODUTO</h4>
-                    </div>	
-                    
-                    
-                    <div class="col-sm-2">
-                        <h4>VALOR DO PRODUTO</h4>
-                    </div>		
-            
-                    <div class="col-sm-3">
-
-                            <h4 class="qntDisponivel text-center" id="#qntcart">QUANTIDADE DISPONIVEL</h4>
-                        
-                    </div>
-            
-                    <div class="col-sm-2 col-xs-offset-right-1">
-                    </div> 
-                </div>
-            </div>
-
-            <div class="col-sm-12">
-                <div class="row">
-                    <div class="col-sm-1 col-sm-offset-1">
                         <img src="imgem/" class="img-responsiva">
                     </div>
                     
                     
-                    <div class="col-sm-4 margin">
-                        <h4>NOME PRODUTO</h4>
+                    <div class="col-sm-4 wishNomeProduto">
+                        <span>NOME PRODUTO</span>
                     </div>	
                     
                     
                     <div class="col-sm-2">
-                        <h4>VALOR DO PRODUTO</h4>
+                        <span>VALOR DO PRODUTO</span>
                     </div>		
             
                     <div class="col-sm-3 quantidadeDisponivel">
 
-                        <h4 class="qntDisponivel text-center" id="#qntcart">QUANTIDADE DISPONIVEL</h4>
+                        <span class="qntDisponivel text-center" id="#qntcart">QUANTIDADE DISPONIVEL</span>
 
                     </div>
             
