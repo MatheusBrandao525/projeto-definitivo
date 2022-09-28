0b4ed9ce38e00151ea170aeea3c69c79504d6595
MatheusBrandao525 <mafe123silva@gmail.com>
2022-09-28 09:45:34 -0400

    Front-end painel de vendas terminado

--- admin/indiceVendas.php
+++ admin/indiceVendas.php
@@ -100,8 +100,8 @@
             <div class="row mostrar-grafico">
                 <div class="col-sm-10 mostra-grafico-01">
                     <div class="box-grafico">
-                        <div class="titlo-indice">
-                            <h5>Ultimas vendas realizadas</h5>
+                        <div class="titlo-grafico">
+                            <h5>Resumo de vendas realizadas</h5>
                         </div><!-- titlo-indice -->
 
                         <div class="row indice-dados espaco-inferior">
@@ -136,17 +136,17 @@
 
                 <div class="col-sm-2 mostra-grafico-02">
                     <div class="centro-indice">
-                        <div class="box-indice-lucro">
-                            <h5>Seu lucro</h5>
-                            <div class="indice-lucro">
+                        <div class="box-grafico01">
+                            <h5>Atual</h5>
+                            <div class="grafico-atual">
                                 <h4>R$ 99.999,00</h4>
                             </div>
                         </div><!-- box-indice-lucro -->
 
-                        <div class="box-indice-grafico">
-                            <h5>Resumo</h5>
-                            <div class="indice-resumo">
-                                <h4>10.736 Vendas <i class="fas fa-chart-line"></i></h4>
+                        <div class="box-grafico02">
+                            <h5>Anterior</h5>
+                            <div class="grafico-anterior">
+                                <h4>R$ 76.980,00</h4>
                             </div>
                         </div><!-- box-indice-grafico -->
                     </div><!-- centro-indice -->
@@ -169,18 +169,18 @@
                 <div class="col-sm-7 mostra-indices-01">
                     <div class="box-indice-ultimas-vendas">
                         <div class="titlo-indice">
-                            <h5>Ultimas vendas realizadas</h5>
+                            <h5>Produtos mais vendidos</h5>
                         </div><!-- titlo-indice -->
 
                         <div class="row indice-dados espaco-inferior">
                             <div class="col-sm-2 dados-venda numero-venda">
-                                <span class="titulo-dados">Nº Venda</span>
+                                <span class="titulo-dados">Ranking</span>
                             </div>
                             <div class="col-sm-4 dados-venda">
-                                <span class="titulo-dados">Tiket</span>
+                                <span class="titulo-dados">Nome Produto</span>
                             </div><div class="col-sm-4 dados-venda">
-                                <span class="titulo-dados">Cliente</span>
-                            </div><div class="col-sm-2 dados-venda">
+                                <span class="titulo-dados">Categoria</span>
+                            </div><div class="col-sm-2 dados-mais-vendido">
                                 <span class="titulo-dados">Valor</span>
                             </div> 
                         </div><!-- indice-dados -->
@@ -193,8 +193,8 @@
                             <div class="col-sm-4 dados-venda">
                                 <a href="#"><span>jk88a78w7hd7a89wu</span></a>
                             </div><div class="col-sm-4 dados-venda">
-                                <span>Fulano01</span>
-                            </div><div class="col-sm-2 dados-venda">
+                                <span>categia-teste</span>
+                            </div><div class="col-sm-2 dados-mais-vendido">
                                 <span>R$ 250,00</span>
                             </div> 
                         </div><!-- indice-dados -->
@@ -205,14 +205,14 @@
                 <div class="col-sm-5 mostra-indices-02">
                     <div class="centro-indice">
                         <div class="box-indice-lucro">
-                            <h5>Seu lucro</h5>
+                            <h5>Nº 1</h5>
                             <div class="indice-lucro">
-                                <h4>R$ 99.999,00</h4>
+                                <h4>Nome Produto</h4>
                             </div>
                         </div><!-- box-indice-lucro -->
 
                         <div class="box-indice-grafico">
-                            <h5>Resumo</h5>
+                            <h5>Total de vendas</h5>
                             <div class="indice-resumo">
                                 <h4>10.736 Vendas <i class="fas fa-chart-line"></i></h4>
                             </div>
@@ -238,18 +238,18 @@
                 <div class="col-sm-7 mostra-indices-01">
                     <div class="box-indice-ultimas-vendas">
                         <div class="titlo-indice">
-                            <h5>Ultimas vendas realizadas</h5>
+                            <h5>Produtos mais buscados</h5>
                         </div><!-- titlo-indice -->
 
                         <div class="row indice-dados espaco-inferior">
                             <div class="col-sm-2 dados-venda numero-venda">
-                                <span class="titulo-dados">Nº Venda</span>
+                                <span class="titulo-dados">Ranking</span>
                             </div>
                             <div class="col-sm-4 dados-venda">
-                                <span class="titulo-dados">Tiket</span>
+                                <span class="titulo-dados">Nome produto</span>
                             </div><div class="col-sm-4 dados-venda">
-                                <span class="titulo-dados">Cliente</span>
-                            </div><div class="col-sm-2 dados-venda">
+                                <span class="titulo-dados">Categoria</span>
+                            </div><div class="col-sm-2 dados-mais-buscado">
                                 <span class="titulo-dados">Valor</span>
                             </div> 
                         </div><!-- indice-dados -->
@@ -260,9 +260,9 @@
                                 <span>Nº 06776</span>
                             </div>
                             <div class="col-sm-4 dados-venda">
-                                <a href="#"><span>jk88a78w7hd7a89wu</span></a>
+                                <a href="#"><span>Produto-teste</span></a>
                             </div><div class="col-sm-4 dados-venda">
-                                <span>Fulano01</span>
+                                <span>Categoria-teste</span>
                             </div><div class="col-sm-2 dados-venda">
                                 <span>R$ 250,00</span>
                             </div> 
@@ -274,16 +274,16 @@
                 <div class="col-sm-5 mostra-indices-02">
                     <div class="centro-indice">
                         <div class="box-indice-lucro">
-                            <h5>Seu lucro</h5>
+                            <h5>Nº 1</h5>
                             <div class="indice-lucro">
-                                <h4>R$ 99.999,00</h4>
+                                <h4>10.736</h4>
                             </div>
                         </div><!-- box-indice-lucro -->
 
                         <div class="box-indice-grafico">
-                            <h5>Resumo</h5>
+                            <h5>Total de buscas</h5>
                             <div class="indice-resumo">
-                                <h4>10.736 Vendas <i class="fas fa-chart-line"></i></h4>
+                                <h4>10.736</h4>
                             </div>
                         </div><!-- box-indice-grafico -->
                     </div><!-- centro-indice -->
